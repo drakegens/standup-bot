2a4d3b7587f4cc4bc799aedc317c51a766de39a8
Drake Gens <drake.gens@goilluminate.com>
2020-03-29 21:56:54 -0500

    Main structure

--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-	"strings"
-
 	"github.com/slack-go/slack"
+	"log"
 )
 
 type ChannelMsg struct {
-	Id    int
-	Name  string
+	Id     int
+	Name   string
 	Fields json.RawMessage
 }
 
@@ -20,71 +18,140 @@ type MemberMsg struct {
 	CorrelatedQuery string `json:"CorrelatedQuery"`
 }
 
-func main() {
+var api *slack.Client
+var rtm *slack.RTM
 
-	token := os.Getenv("SLACK_API_KEY")
-	api := slack.New(token)
+func getStandUpUpdateFromUser(memberId string) string {
 
-	rtm := api.NewRTM()
-	go rtm.ManageConnection()
+	var update = "Yesterday: "
 
-	channels, err := api.GetChannels(true)
-	if err != nil {
-		log.Fatalf("%s: %s", "Unable to get channels", err)
-	}
+	_, _, channelId, _ := api.OpenIMChannel(memberId)
+	//go func() {
+	rtm.SendMessage(rtm.NewOutgoingMessage("What did you do yesterday?", channelId))
 
-	var memberIds []string
-//	userId := "hello"
-	for _, channel := range channels {
-		fmt.Println(channel.Name)
-		if channel.Name == "standup"{
-			memberIds = channel.Members
-			//userId = channel.ID
-			break
-		}
-	}
-
-	fmt.Println(memberIds)
-	//memberId := memberIds[0]
-
-	for _, memberId := range memberIds {
-		fmt.Println(memberId, "hey")
-		_, _, channelId, _ := api.OpenIMChannel(memberId)
-		rtm.SendMessage(rtm.NewOutgoingMessage("What did you do yesterday?", channelId))
-	}
-
-	//api.GetUsers()
-	//
-	//api.PostMessage()
-Loop:
-	for {
-		select {
-		case msg := <-rtm.IncomingEvents:
-			fmt.Print("Event Received: ")
+Loop1:
+	select {
+	case msg := <-rtm.IncomingEvents:
+		fmt.Print("Event Received: ")
+		for {
 			switch ev := msg.Data.(type) {
 			case *slack.ConnectedEvent:
 				fmt.Println("Connection counter:", ev.ConnectionCount)
 
 			case *slack.MessageEvent:
-				fmt.Printf("Message: %v\n", ev)
-				fmt.Printf("Received a message!!!!!")
-				info := rtm.GetInfo()
-				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
-
-				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
-					rtm.SendMessage(rtm.NewOutgoingMessage("What's up buddy!?!?", ev.Channel))
-				}
+				update += ev.Text
+				fmt.Printf("Message: %v\n", ev.Text)
 
 			case *slack.RTMError:
 				fmt.Printf("Error: %s\n", ev.Error())
 
 			case *slack.InvalidAuthEvent:
 				fmt.Printf("Invalid credentials")
-				break Loop
+				break Loop1
 
 			default:
 				//Take no action
 			}
 		}
 	}
+	rtm.SendMessage(rtm.NewOutgoingMessage("What are you going to do today?", channelId))
+Loop2:
+	select {
+	case msg := <-rtm.IncomingEvents:
+		fmt.Print("Event Received: ")
+		for {
+			switch ev := msg.Data.(type) {
+			case *slack.ConnectedEvent:
+				fmt.Println("Connection counter:", ev.ConnectionCount)
+
+			case *slack.MessageEvent:
+				update += "\nToday:" + ev.Text
+				fmt.Printf("Message: %v\n", ev.Text)
+
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop2
+
+			default:
+				//Take no action
+			}
+		}
+	}
+
+	//	}()
+	return update
+
+}
+
+func main() {
+	token := os.Getenv("SLACK_API_KEY")
+	api := slack.New(token)
+	rtm := api.NewRTM()
+
+	go rtm.ManageConnection()
+
+	channels, err := api.GetChannels(true)
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Unable to get channels", err)
+	}
+
+	var memberIds []string
+	var standupChannelId string
+
+	for _, channel := range channels {
+		fmt.Println(channel.Name)
+		if channel.Name == "standup" {
+			memberIds = channel.Members
+			standupChannelId = channel.ID
+			fmt.Println(standupChannelId)
+			break
+		}
+	}
+
+	var standupUpdates [10]string
+
+	for i, memberId := range memberIds {
+		standupUpdates[i] = getStandUpUpdateFromUser(memberId)
+	}
+
+	rtm.SendMessage(rtm.NewOutgoingMessage(standupUpdates[0], standupChannelId))
+
+	//api.GetUsers()
+	//
+	//api.PostMessage()
+	//Loop:
+	//	for {
+	//		select {
+	//		case msg := <-rtm.IncomingEvents:
+	//			fmt.Print("Event Received: ")
+	//			switch ev := msg.Data.(type) {
+	//			case *slack.ConnectedEvent:
+	//				fmt.Println("Connection counter:", ev.ConnectionCount)
+	//
+	//			case *slack.MessageEvent:
+	//				fmt.Printf("Message: %v\n", ev)
+	//				fmt.Printf("Received a message!!!!!")
+	//				info := rtm.GetInfo()
+	//				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
+	//
+	//				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
+	//					rtm.SendMessage(rtm.NewOutgoingMessage("What's up buddy!?!?", ev.Channel))
+	//				}
+	//
+	//			case *slack.RTMError:
+	//				fmt.Printf("Error: %s\n", ev.Error())
+	//
+	//			case *slack.InvalidAuthEvent:
+	//				fmt.Printf("Invalid credentials")
+	//				break Loop
+	//
+	//			default:
+	//				//Take no action
+	//			}
+	//		}
+	//	}
 }
